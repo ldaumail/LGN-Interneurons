9b4b1037ed45f51bd606cce94e2e433fdbc4eae0
ldaumail <49167117+ldaumail@users.noreply.github.com>
2021-09-30 18:18:39 -0500

    Add files via upload

---
+++ main_pipeline.m
@@ -0,0 +1,139 @@
+% This script attempts to process single unit data from monocular and
+% binocular stimulation at multiple contrast levels to detect binocularly modulated single
+% units. The goal is then to look at the corresponding multi-unit activity
+% surrounding those modulated channels, as modulated single units may be in
+% fact modulated by interneurons (inhibitory neurons) that could be
+% detected in the MUA
+% Written by Loic Daumail, last updated on 09/30/2021
+
+
+%% Pre-start: use the list of single units file names that were selected in the adaptation analysis with
+% high contrast
+selectUnitsFilenames =load('C:\Users\daumail\OneDrive - Vanderbilt\Documents\LGN_data_042021\single_units\s_potentials_analysis\analysis\single_units_ns6_metadata.mat');
+filenames = selectUnitsFilenames.STIMFileName;
+
+
+unitsDir = 'C:\Users\daumail\OneDrive - Vanderbilt\Documents\LGN_data_042021\single_units\inverted_power_channels\good_single_units_data_4bumps_more\new_peak_alignment_anal\';
+unitsDataDir = [unitsDir 'refined_dataset']; 
+unitsData= load(unitsDataDir);
+
+
+cellClass = {'K','M','P','K','K','K','M','P','P','','M','M','','','M','','','P','','M','','M','M','','P','M','','P', ...
+'P','','','K','P','M','M','M','P','','P','K','P','P','','P','P','M','','P','M','P','M','P','','P','M','M','P','','M','M','P','M', ...
+'','','M','M','M','P','M','M','M','M','P','P'};
+cellClass([1,46,55]) = [];
+
+directory = 'C:\Users\daumail\OneDrive - Vanderbilt\Documents\loic_code_042021\lgn_interneuron_suppression';
+addpath(genpath(directory))
+
+%% contrast levels encountered in the dominant eye
+
+ allContLevels =[];
+ for i =1:71
+     if ~isempty(filenames{i})
+         contLevels = unique(unitsData.new_data(i).channel_data.contrast);
+         allContLevels = unique([allContLevels; contLevels]);
+     end
+ end
+ 
+ 
+%% I. Get selected trials data
+
+selected_data = suaTrialSelection(unitsData, filenames);%get 
+
+allfilename = 'C:\Users\daumail\OneDrive - Vanderbilt\Documents\LGN_data_042021\single_units\lgn_interneuron_suppression\all_cont_lgn_data_09292021';
+save(strcat(allfilename, '.mat'), 'selected_data');
+
+%% II. Test for binocular modulation
+
+%relevant comparisons:
+%(]0:0.1] NDE && ]0:0.1] DE) vs (0 NDE && ]0.01]DE)
+%(]0.1:0.3] NDE && ]0:0.1] DE) vs (0 NDE && ]0.01]DE)
+%(]0.3:0.5] NDE && ]0:0.1] DE) vs (0 NDE && ]0.01]DE)
+%(]0.7:1] NDE && ]0:0.1] DE) vs (0 NDE && ]0.01]DE)
+
+%Repeat for ]0.1:0.3] DE, ]0.3:0.5] DE,]0.7:1] DE ===> total of 16 tests
+
+
+lowContNDE = [0, 10, 30, 70];
+lowContDE = [0, 10, 30, 70];
+
+%binoconds:
+binoConds = combvec(lowContDE,lowContNDE);
+monoConds = binoConds(1,:);
+
+sigResults = binocularModulationPowerTest(selected_data, binoConds, monoConds);
+allfilename = 'C:\Users\daumail\OneDrive - Vanderbilt\Documents\LGN_data_042021\single_units\lgn_interneuron_suppression\binmod_lgn_power_test_09292021';
+save(strcat(allfilename, '.mat'), 'sigResults');
+
+%% I'. & II'.Try with more pooling of contrast levels to save trials
+selected_data_pooled = suaTrialSelectionContPool(unitsData, filenames);%get 
+allfilename = 'C:\Users\daumail\OneDrive - Vanderbilt\Documents\LGN_data_042021\single_units\lgn_interneuron_suppression\all_cont_lgn_data_pooled_09292021';
+save(strcat(allfilename, '.mat'), 'selected_data_pooled');
+
+lowContNDE = [50];
+lowContDE = [0];
+
+%binoconds:
+binoConds = combvec(lowContDE,lowContNDE);
+monoConds = binoConds(1,:);
+
+sigPooledResults = binocularModulationPowerTest(selected_data_pooled, binoConds, monoConds);
+allfilename = 'C:\Users\daumail\OneDrive - Vanderbilt\Documents\LGN_data_042021\single_units\lgn_interneuron_suppression\binmod_lgn_pooled_power_test_09292021';
+save(strcat(allfilename, '.mat'), 'sigPooledResults');
+%% Plot mean response of modulated data 
+
+%% III. Get MUA corresponding to the modulated SUA
+
+%get modulated single units indices
+allfilename = 'C:\Users\daumail\OneDrive - Vanderbilt\Documents\LGN_data_042021\single_units\lgn_interneuron_suppression\binmod_lgn_pooled_power_test_09292021';
+sigPooledResults = load(strcat(allfilename, '.mat'));
+sigBinoModul = sigPooledResults.sigPooledResults;
+sigBinoModul(isnan(sigBinoModul)) = 0;
+sigBinoModulIdx = find(sigBinoModul);
+
+%get modulated single units filenames and channel number
+allfilename = 'C:\Users\daumail\OneDrive - Vanderbilt\Documents\LGN_data_042021\single_units\lgn_interneuron_suppression\all_cont_lgn_data_pooled_09292021';
+selected_data = load(strcat(allfilename, '.mat'));
+selected_data =selected_data.selected_data_pooled;
+xfilenames = fieldnames(selected_data);
+modulxFilenames = xfilenames(sigBinoModulIdx);
+
+channel = cell(length(xfilenames),1);
+for i = 1:length(xfilenames)
+    channel{i} = selected_data.(xfilenames{i}).chan; 
+end
+modulChannel = channel(sigBinoModulIdx);
+
+%Load MUA and select corresponding MUA
+%{
+%MUA files were obtained with "mua_adaptation_analysis.m" 
+muaChannelsdir = 'C:\Users\daumail\OneDrive - Vanderbilt\Documents\LGN_data_042021\multi_units\selected_channels\';
+muaFilenames = dir(strcat(muaChannelsdir, '*_DE*')); %only include the DE files, as contrast range differs between eyes stimulated. If we use the CRFs to label channels, we will need a wide range of contrasts ==> "DE' files
+
+%create data structure to select trial responses
+unitsData = struct();
+
+for i = 1: length(muaFilenames)
+    filename = muaFilenames(i).name;
+    MUA_datafile = load(strcat(muaChannelsdir, filename));
+    unitsData.new_data(i).channel_data = MUA_datafile;
+end
+%}
+
+%transfer necessary .ns6 files from TEBA onto main disk
+for i = 2:length(modulxFilenames)
+%i =1;
+selectxFile = modulxFilenames{i};
+selectDate = selectxFile(2:9);
+ns6Dir = strcat('E:\LGN_data_TEBA\rig021\', selectDate);
+
+if exist(ns6Dir, 'dir')
+    mkdir(strcat('C:\Users\daumail\OneDrive - Vanderbilt\Documents\LGN_data_042021\single_units\lgn_interneuron_suppression\ns6_bino_modul\', selectDate))
+    sourcedir = ns6Dir;
+    destdir = strcat('C:\Users\daumail\OneDrive - Vanderbilt\Documents\LGN_data_042021\single_units\lgn_interneuron_suppression\ns6_bino_modul\',selectDate);
+end
+[status, msg] = copyfile( strcat(sourcedir,'\*cinterocdrft*.ns6'),  destdir);
+end
+%end
+
