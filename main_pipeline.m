bde0c3188d5d233fdf06a1e40a27526c02ce7062
ldaumail <49167117+ldaumail@users.noreply.github.com>
2021-10-07 14:41:54 -0500

    Add files via upload

--- main_pipeline.m
+++ main_pipeline.m
@@ -121,155 +121,102 @@ for i = 1: length(muaFilenames)
 end
 %}
 
-%transfer necessary .ns6 files from TEBA onto main disk
-for i = 1:length(modulxFilenames)
-    
-    selectxFile = modulxFilenames{i};
-    selectDate = selectxFile(2:9);
-    ns6Dir = strcat('E:\LGN_data_TEBA\rig021\', selectDate);
-    %ns6Dir = strcat('E:\LGN_data_TEBA\rig021_2\', selectDate);
-    %ns6Dir = strcat('E:\LGN_data_TEBA\rig022_2\', selectDate);
-     %ns6Dir = strcat('E:\LGN_data_TEBA\rig022\', selectDate);
-    if exist(ns6Dir, 'dir')
-        %mkdir(strcat('C:\Users\daumail\OneDrive - Vanderbilt\Documents\LGN_data_042021\single_units\lgn_interneuron_suppression\ns6_bino_modul\', selectDate))
-        sourcedir = ns6Dir;
-        destdir = strcat('C:\Users\daumail\OneDrive - Vanderbilt\Documents\LGN_data_042021\single_units\lgn_interneuron_suppression\ns6_bino_modul\',selectDate);
-   %[status, msg] = copyfile( strcat(sourcedir,'\*cinterocdrft*.ns6'),  destdir);
-     [status, msg] = copyfile( strcat(sourcedir,'\*cinterocdrft*.nev'),  destdir);
+%transfer necessary .ns6, .nev, .text (.gCINTEROCDRFTGrating_di) and .bhv files from TEBA onto main disk
+PATHS = {'E:\LGN_data_TEBA\rig021\', 'E:\LGN_data_TEBA\rig021_2\', 'E:\LGN_data_TEBA\rig022\', 'E:\LGN_data_TEBA\rig022_2\'};
+for p =1:length(PATHS)
+    for i = 1:length(modulxFilenames)
+        selectxFile = modulxFilenames{i};
+        selectDate = selectxFile(2:9);
+        ns6Dir = strcat(PATHS{p}, selectDate);
+        if exist(ns6Dir, 'dir')
+            %mkdir(strcat('C:\Users\daumail\OneDrive - Vanderbilt\Documents\LGN_data_042021\single_units\lgn_interneuron_suppression\ns6_bino_modul\', selectDate))
+            sourcedir = ns6Dir;
+            destdir = strcat('C:\Users\daumail\OneDrive - Vanderbilt\Documents\LGN_data_042021\single_units\lgn_interneuron_suppression\ns6_bino_modul\',selectDate);
+            %[status, msg] = copyfile( strcat(sourcedir,'\*cinterocdrft*.ns6'),  destdir);
+            %[status, msg] = copyfile( strcat(sourcedir,'\*cinterocdrft*.nev'),  destdir);
+            %[status, msg] = copyfile( strcat(sourcedir,'\*.gCINTEROCDRFTGrating_di'),  destdir);
+            [status, msg] = copyfile( strcat(sourcedir,'\*cinterocdrft*.bhv'),  destdir);
+        end
     end
-   
 end
 
-
-%% Process .ns6 files to get the MUA and the right trial indices
+%% IV Process .ns6 files to get the MUA and the right trial indices
 
 % (1) Get MUA from .ns6 files 
 %.ns6 file directory
-selectxFile = modulxFilenames{1}; %try with one file
-selectDate = selectxFile(2:9);
-dataDir = strcat('C:\Users\daumail\OneDrive - Vanderbilt\Documents\LGN_data_042021\single_units\lgn_interneuron_suppression\ns6_bino_modul\',selectDate,'\');
-ns6Filenames = dir(strcat(dataDir, '*cinterocdrft*.ns6'));
-stackedMUA = struct();
-for fn = 1:length({ns6Filenames.name})
-    ns6_filename = ns6Filenames(fn).name;
-    clear ext NS_header banks neural
-    % Read in NS Header
-    ext          = 'ns6';
-    NS_Header    = openNSx(strcat(dataDir,ns6_filename),'noread');
-    el  = 'eD';
-    % get basic info about recorded data
-    neural       = strcmp({NS_Header.ElectrodesInfo.ConnectorBank},el(2)); % logicals where contact bank name matches electrode of interest
-    N.neural     = sum(neural); % number of neural channels
-    NeuralLabels = {NS_Header.ElectrodesInfo(neural).Label}; %get labels
-    Fs           = NS_Header.MetaTags.SamplingFreq; % get sampling frequency
-    nyq          = Fs/2;
-    r            = Fs/1000;
-    %r2           = Fs/30000;
+directory = 'C:\Users\daumail\OneDrive - Vanderbilt\Documents\loic_code_042021\lgn_interneuron_suppression';
+addpath(genpath(directory))
 
-    % counters
-    clear nct
-    nct = 0;
-
-    tic
-    % process data electrode by electrode
-    for e = 1:length(neural)
-        if neural(e) == 1    % why? because neural is a vector of logicals, 1 = contacts we want
-            nct = nct+1;
-
-            % open data for this channel.
-            clear NS DAT
-            electrode = sprintf('c:%u',e);
-            NS        = openNSx(strcat(dataDir,ns6_filename),electrode,'read','uV');
-            DAT       = NS.Data; NS.Data = [];  % this is the whole signal on one channel, 30 kHz!
-
-
-            % preallocate data matrices
-            if nct == 1
-                N.samples = length(DAT);
-                %LFP       = zeros(ceil(N.samples/r),N.neural); % preallocating for downsampled data
-                MUA      = zeros(ceil(N.samples/r),N.neural);
-            end
-        % extract the MUA:
-            clear hpc hWn bwb bwa hpMUA
-            hpc       = 750;  %high pass cutoff
-            hWn       = hpc/nyq;
-            [bwb,bwa] = butter(4,hWn,'high');
-            hpMUA     = filtfilt(bwb,bwa,double(DAT)); %high pass filter
-
-            % low pass at 5000 Hz and rectify 
-            clear lpc lWn bwb bwa 
-            lpc       = 5000;  % cutoff
-            lWn       = lpc/nyq;
-            [bwb,bwa] = butter(4,lWn,'low');
-            hpMUA     = abs(filtfilt(bwb,bwa,hpMUA)); %low pass filter &rectify
-
-            % low pass filter at x Hz. 
-            clear lpc lWn bwb bwa lpMUA
-            lpc       = 200; %low pass cutoff
-            lWn       = lpc/nyq;
-            [bwb,bwa] = butter(4,lWn,'low'); 
-            lpMUA     = filtfilt(bwb,bwa,hpMUA);  %low pass filter to smooth
-
-
-            % decimate both LFP and analog MUA (aMUA) to get 1kHz samp freq
-            MUA(:,nct) = decimate(lpMUA,r); 
-            
-            % high pass filter
-             %{
-            clear hpc hWn bwb bwa hpMUA
-            hpc       = 250; %high pass cutoff
-            hWn       = hpc/nyq;
-            [bwb,bwa] = butter(4,hWn,'high');
-            hpsLFP      = filtfilt(bwb,bwa,DAT);  %low pass filter
-            %}
-            % decimate sLFP to get 20kHz samp freq
-            %LFP(:,nct) = decimate(hpsLFP,r2);
-
-            clear DAT
-
-        end
-
-    end
-    toc
-    % Warning! THESE DATA ARE IN THE SAME ORDER AS THE BR PINS, NOT THE ORDER OF THE PROBE
-    %As the .ns6 data was retrieved using openNSx() and not getLFP(), we need
-    %to sort the channels ourselves. With getLFP(), sorting is done
-    %automatically
-    % sort data from top of electrode to bottom.
-
-    % get indices
-    idx = zeros(1,length(NeuralLabels));
-    for i = 1:length(NeuralLabels)
-
-        Str  = cell2mat(NeuralLabels(i));
-        Key   = 'eD';
-        Str(strfind(Str, '%02d')) = [];
-
-        Index = strfind(Str, Key);
-        idx(1, i) = sscanf(Str(Index(1) + length(Key):end), '%g', 1);
-
-    end
-    Srt_MUA = nan(length(MUA(:,1)), length(NeuralLabels));
-   %apply sorting
-    Srt_MUA(:,idx) = MUA(:,:);
-    sortedLabels = NeuralLabels(idx);
-    
-    mua_filename = strcat('x',ns6_filename(1:end-4));
-    stackedMUA.(mua_filename).MUA = Srt_MUA;
-    stackedMUA.(mua_filename).sortedLabels = sortedLabels;
-
-end
+selFilenames = modulxFilenames([4;9:11]); %also need to do #8 but there is a bug
+rectMUA = rectifiedMUA(selFilenames, modulChannel);
 
 %mkdir(strcat('C:\Users\daumail\OneDrive - Vanderbilt\Documents\LGN_data_042021\single_units\lgn_interneuron_suppression\mua_bino_modul\', selectDate))    
 %filtdir =strcat('C:\Users\daumail\OneDrive - Vanderbilt\Documents\LGN_data_042021\single_units\lgn_interneuron_suppression\mua_bino_modul\', selectDate);
 %save(strcat(filtdir,'_cinterocdrft_750hzhighpass_1khz_MUA.mat'), 'stackedMUA','-v7.3');
 
-%% LOAD EVENT TIMES/CODES TO TRIGGER DATA
+%%
+%npmkdir    = 'C:\Users\maier\Documents\MATLAB\NPMK-master\'; 
+%nbanadir   = 'C:\Users\maier\Documents\bootcamp-selected\nbanalysis\'; 
+%addpath(genpath(dataDir))
+%addpath(genpath(npmkdir))
+%addpath(genpath(nbanadir))
+penetrations = cell(length(modulxFilenames),1);
+for i =1:length(modulxFilenames)
+    fname = modulxFilenames{i};
+    penetrations{i} = fname(2:13);
+end
+%for i = 1: length(unique(penetrations)) %number of sessions
+selectFile = penetrations{1}; %try with one file, but use 'i' index otherwise !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+selectDate = selectFile(1:8);
+dataDir = strcat('C:\Users\daumail\OneDrive - Vanderbilt\Documents\LGN_data_042021\single_units\lgn_interneuron_suppression\ns6_bino_modul\',selectDate,'\');
+
+textFilenames = dir(strcat(dataDir, '*.gCINTEROCDRFTGrating_di'));
+%for n = 1:length(textFilenames.name)
+BRdatafile = textFilenames(1).name(1:24); %try with first file use 'n' index !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+filename   = [dataDir BRdatafile]; 
+
+
+%% V. LOAD STIMULUS CONDITIONS with text file 
+
+patterns   = {'rforidrft','rfsfdrft','posdisparitydrft','disparitydrft','cinterocdrft','coneinterocdrft','conedrft', ...
+                'colorflicker','bwflicker','rfori','rfsize','cinteroc','color','rfsf','mcosinteroc','dotmapping'}; 
+clear p match
+for p = 1:length(patterns)
+
+   pattern      = patterns{p}; 
+  
+   if any(strfind(BRdatafile,pattern))
+       startlog = strfind(BRdatafile,pattern); 
+       if ~isequal(BRdatafile(startlog:end-3),pattern),continue
+       else
+       match    = patterns{p}; 
+       end
+   end
+   
+end
+
+if isequal(match,'dotmapping')
+ext  = '.gDotsXY_di';
+else
+ext  = ['.g' upper(match) 'Grating_di']; 
+end
+
+if contains(ext,'DRFT')
+      grating     = readgDRFTGrating([filename ext]); % from nbanalysis (or even MLAnalysisOnline--might be out of date)
+elseif contains(ext,'Dots')
+      grating     = readgDotsXY([filename ext]);
+else
+      grating     = readgGrating([filename ext]);
+end
+
+
+%% VI. LOAD EVENT TIMES/CODES TO TRIGGER DATA
 %The NEV files contain the digital event codes that are sent from MonkeyLogic to the 
 %BlackRock machine. They are sent for different stimulus/behavioral events, 
 %including when the stimuli appear on the screen. 
- 
-NEV             = openNEV([filename '.nev'],'noread','overwrite');
+nevFilenames = dir(strcat(dataDir, '*cinterocdrft*.nev'));  
+nevfilename = strcat(dataDir, nevFilenames(1).name); %use 'n' index !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+NEV             = openNEV(nevfilename ,'noread','overwrite');
 
 %The codes themselves can found in this field after subtracting 128 (unknown quirk). 
 %Good codes to know are 23, 25, 27, 29, 31 (Stimulus Onsets—23 
@@ -299,12 +246,23 @@ EventTimes      = floor(NEV.Data.SerialDigitalIO.TimeStampSec.*1000); % convert 
 
 STIM            = sortStimandTimeData(grating,pEvC,pEvT,'stim'); % this is in nbanalysis. definitely double check it before you use it. 
 
-%% Trigger MUA to behavioral event codes
+%% VII. Trigger MUA to behavioral event codes
+muaDir = 'C:\Users\daumail\OneDrive - Vanderbilt\Documents\LGN_data_042021\single_units\lgn_interneuron_suppression\mua_bino_modul\';
+stacked_MUA = load(strcat(muaDir,sprintf('%s_cinterocdrft_750hzhighpass_1khz_MUA.mat',selectFile)));
+muaFiles = fieldnames(stacked_MUA.stackedMUA);
+pre   = -600;
+post  = 1300; 
+%!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+STIM.aMUA = trigData(stacked_MUA.stackedMUA.(muaFiles{1}).MUA,floor(STIM.onsets./30),-pre,post); % use 'n' index !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-pre   = -50;
-post  = 300; 
 
-STIM.aMUA = trigData(Srt_MUA,floor(STIM.onsets./30),-pre,post); 
+%% VIII. re-trigger MUA to photodiode = use photoRetriggerSTIM..?
+TP = repmat([pre post], length(STIM.trial),1);
+STIM.photoMUA = photoReTrigger(TP, filename,STIM.ypos, STIM);
 
 
-%% re-trigger MUA to photodiode = use photoRetriggerSTIM..?
+
+
+
+
+
