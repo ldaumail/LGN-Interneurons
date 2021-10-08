570dc2a339b41595e97ed71d158dfd8f811aef22
ldaumail <49167117+ldaumail@users.noreply.github.com>
2021-10-07 22:27:53 -0500

    Add files via upload

--- main_pipeline.m
+++ main_pipeline.m
@@ -155,7 +155,8 @@ rectMUA = rectifiedMUA(selFilenames, selChan);
 %filtdir =strcat('C:\Users\daumail\OneDrive - Vanderbilt\Documents\LGN_data_042021\single_units\lgn_interneuron_suppression\mua_bino_modul\', selectDate);
 %save(strcat(filtdir,'_cinterocdrft_750hzhighpass_1khz_MUA.mat'), 'stackedMUA','-v7.3');
 
-%%
+%% Trigger data to event codes and retrigger to photodiode
+
 %npmkdir    = 'C:\Users\maier\Documents\MATLAB\NPMK-master\'; 
 %nbanadir   = 'C:\Users\maier\Documents\bootcamp-selected\nbanalysis\'; 
 %addpath(genpath(dataDir))
@@ -166,102 +167,8 @@ for i =1:length(modulxFilenames)
     fname = modulxFilenames{i};
     penetrations{i} = fname(2:13);
 end
-%for i = 1: length(unique(penetrations)) %number of sessions
-selectFile = penetrations{1}; %try with one file, but use 'i' index otherwise !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-selectDate = selectFile(1:8);
-dataDir = strcat('C:\Users\daumail\OneDrive - Vanderbilt\Documents\LGN_data_042021\single_units\lgn_interneuron_suppression\ns6_bino_modul\',selectDate,'\');
-
-textFilenames = dir(strcat(dataDir, '*.gCINTEROCDRFTGrating_di'));
-%for n = 1:length(textFilenames.name)
-BRdatafile = textFilenames(1).name(1:24); %try with first file use 'n' index !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-filename   = [dataDir BRdatafile]; 
-
-
-%% V. LOAD STIMULUS CONDITIONS with text file 
-
-patterns   = {'rforidrft','rfsfdrft','posdisparitydrft','disparitydrft','cinterocdrft','coneinterocdrft','conedrft', ...
-                'colorflicker','bwflicker','rfori','rfsize','cinteroc','color','rfsf','mcosinteroc','dotmapping'}; 
-clear p match
-for p = 1:length(patterns)
-
-   pattern      = patterns{p}; 
-  
-   if any(strfind(BRdatafile,pattern))
-       startlog = strfind(BRdatafile,pattern); 
-       if ~isequal(BRdatafile(startlog:end-3),pattern),continue
-       else
-       match    = patterns{p}; 
-       end
-   end
-   
-end
-
-if isequal(match,'dotmapping')
-ext  = '.gDotsXY_di';
-else
-ext  = ['.g' upper(match) 'Grating_di']; 
-end
-
-if contains(ext,'DRFT')
-      grating     = readgDRFTGrating([filename ext]); % from nbanalysis (or even MLAnalysisOnline--might be out of date)
-elseif contains(ext,'Dots')
-      grating     = readgDotsXY([filename ext]);
-else
-      grating     = readgGrating([filename ext]);
-end
-
-
-%% VI. LOAD EVENT TIMES/CODES TO TRIGGER DATA
-%The NEV files contain the digital event codes that are sent from MonkeyLogic to the 
-%BlackRock machine. They are sent for different stimulus/behavioral events, 
-%including when the stimuli appear on the screen. 
-nevFilenames = dir(strcat(dataDir, '*cinterocdrft*.nev'));  
-nevfilename = strcat(dataDir, nevFilenames(1).name); %use 'n' index !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-NEV             = openNEV(nevfilename ,'noread','overwrite');
-
-%The codes themselves can found in this field after subtracting 128 (unknown quirk). 
-%Good codes to know are 23, 25, 27, 29, 31 (Stimulus Onsets—23 
-%is first stimulus on in a trial, 25 is second stimulus on in a trial, etc). 
-%Stimulus offsets are the evens in that range—24, 26, 28, 30, 32. 
-%The starts of trials are signaled by 999 and the ends by 18 18 18. 
-%There are others in there as well. 
-
-EventCodes      = NEV.Data.SerialDigitalIO.UnparsedData - 128;        % we don't know whywe have to subtract 128 but we do
-%The time the code was sent can be found in this field. 
-%This is in samples (at the 30kHz rate). The Times on the next line are in seconds. 
-%We multiply by 1000 to get into ms. 
- 
-EventSamples    = NEV.Data.SerialDigitalIO.TimeStamp;                 % in samples 
-EventTimes      = floor(NEV.Data.SerialDigitalIO.TimeStampSec.*1000); % convert to ms 
-
-%Checkout the parsEventCodesML function. It takes those 999s and 18 18 18s and 
-%breaks up the codes/times into separate trials.
-%Rfori files typically contain 5 stimulus presentations per trial.
-
-[pEvC, pEvT]    = parsEventCodesML(EventCodes,EventSamples);          % sorts codes, samps or times into trials
-
-
-% So far all of these data are from EVERY trial, including trials where
-% animal breaks fixation. Lets get rid of those and make a new structure
-% with the grating info and the stimulus onsets 
-
-STIM            = sortStimandTimeData(grating,pEvC,pEvT,'stim'); % this is in nbanalysis. definitely double check it before you use it. 
-
-%% VII. Trigger MUA to behavioral event codes
-muaDir = 'C:\Users\daumail\OneDrive - Vanderbilt\Documents\LGN_data_042021\single_units\lgn_interneuron_suppression\mua_bino_modul\';
-stacked_MUA = load(strcat(muaDir,sprintf('%s_cinterocdrft_750hzhighpass_1khz_MUA.mat',selectFile)));
-muaFiles = fieldnames(stacked_MUA.stackedMUA);
-pre   = -600;
-post  = 1300; 
-%!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-STIM.aMUA = trigData(stacked_MUA.stackedMUA.(muaFiles{1}).MUA,floor(STIM.onsets./30),-pre,post); % use 'n' index !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
-
-%% VIII. re-trigger MUA to photodiode = use photoRetriggerSTIM..?
-TP = [STIM.trstart STIM.trend];
-[newTP, trigger{i,n}, STIM]= photoReTrigger(TP, filename,STIM.ypos,{}, STIM);
-
-
+dataDir =  'C:\Users\daumail\OneDrive - Vanderbilt\Documents\LGN_data_042021\single_units\lgn_interneuron_suppression\';
+[all_photoMUA] = photoTrigMUA(penetrations, dataDir);
 
 
 
